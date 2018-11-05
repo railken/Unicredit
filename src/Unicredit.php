ca237ae91d3093344f159e9543a7b964a912933f
Reza Marandi <reza.marandi@live.com>
2018-11-06 01:05:54 +0330

    Fixed null checking of response object

--- src/Unicredit.php
+++ src/Unicredit.php
@@ -119,21 +119,20 @@ class Unicredit
 
         $response = new Bag();
 
-        if ($verify->paymentID !== null) {
-            $response->ok = true;
-            $response->status = 'ok';
-            $response->payment_id = $verify->paymentID;
-            $response->transaction_id = $verify->tranID;
-
-            return $response;
-        }
-
-        if ($verify->error !== null) {
+        if (!empty($verify->error) && $verify->error) {
             $response->ok = false;
             $response->status = 'ko';
             $response->error = new Bag();
             $response->error->code = $verify->error;
             $response->error->description = $verify->errorDesc;
+            return $response;
+        }
+
+        if (!empty($verify->paymentID)) {
+            $response->ok = true;
+            $response->status = 'ok';
+            $response->payment_id = $verify->paymentID;
+            $response->transaction_id = $verify->tranID;
             return $response;
         }
     }
